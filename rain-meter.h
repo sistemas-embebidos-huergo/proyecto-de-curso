b6ab482a6146e8c4a29d21b4a97df1ef10a3c8cd
Sistemas Embebidos <fluzko@huergo.edu.ar>
2025-06-25 14:22:00 -0300

    Create rain-meter.h

---
+++ rain-meter.h
@@ -0,0 +1,20 @@
+```cpp
+#ifndef RAIN_METER_H
+#define RAIN_METER_H
+
+#include <Arduino.h>
+
+class RainMeter {
+private:
+  int analogPin;
+  int digitalPin;
+
+public:
+  RainMeter(int analogPin, int digitalPin);
+  void begin();
+  bool isRaining();
+  int rainIntensity();
+};
+
+#endif
+```
